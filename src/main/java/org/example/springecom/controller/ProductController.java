45bfbba1ee34c994e2206666f835b1c3f065e630
IT22074072 <it22074072@my.sliit.lk>
2025-01-14 17:52:08 +0530

    fetching all products from db

--- src/main/java/org/example/springecom/controller/ProductController.java
+++ src/main/java/org/example/springecom/controller/ProductController.java
@@ -1,9 +1,27 @@
 package org.example.springecom.controller;
 
+import org.example.springecom.model.Product;
+import org.example.springecom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
+@CrossOrigin //allow to accept requests from any frontend port
 public class ProductController {
 
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/products")
+    public List<Product> getProducts(){
+        List<Product> products = service.getAllProducts(); // Log the response
+        return products;
+    }
 
 }
